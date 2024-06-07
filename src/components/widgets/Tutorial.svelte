7881259579f09ab94572643a8f0bbd1750325739
Aditya Deshmukh <adityad1405@gmail.com>
2024-06-08 04:30:25 +0530

    Initial commit

---
+++ src/components/widgets/Tutorial.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { Tile } from "../board";
+	export let visible: boolean;
+</script>
+
+<h3>how to play</h3>
+<div>You already know how to play Wordle Sneha :P</div>
+
+<div class:complete={visible} class="examples">
+	<div><strong>Examples</strong></div>
+	<div class="row">
+		<Tile value="w" state="🟩" />
+		<Tile value="e" state="🔳" />
+		<Tile value="a" state="🔳" />
+		<Tile value="r" state="🔳" />
+		<Tile value="y" state="🔳" />
+	</div>
+	<div>The letter <strong>W</strong> is in the word and in the correct spot.</div>
+	<div class="row">
+		<Tile value="p" state="🔳" />
+		<Tile value="i" state="🟨" />
+		<Tile value="l" state="🔳" />
+		<Tile value="l" state="🔳" />
+		<Tile value="s" state="🔳" />
+	</div>
+	<div>The letter <strong>I</strong> is in the word but in the wrong spot.</div>
+	<div class="row">
+		<Tile value="v" state="🔳" />
+		<Tile value="a" state="🔳" />
+		<Tile value="g" state="🔳" />
+		<Tile value="u" state="⬛" />
+		<Tile value="e" state="🔳" />
+	</div>
+	<div>The letter <strong>U</strong> is not in the word in any spot.</div>
+</div>
+<div>
+	I really love you bubs, I know I'm busy sometimes so you can just pass the time playing Wordle for as long as you want baby.
+</div>
+
+<style lang="scss">
+	div {
+		margin: 14px 0;
+	}
+	.examples {
+		border-top: 1px solid var(--border-primary);
+		border-bottom: 1px solid var(--border-primary);
+		:global(.row > *) {
+			height: 100%;
+			aspect-ratio: 1;
+		}
+		&:not(.complete) :global(.row .back) {
+			transition-delay: 0.3s;
+		}
+	}
+	.row {
+		height: 40px;
+		display: flex;
+		gap: 4px;
+	}
+</style>
