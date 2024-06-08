d720e8e00014fbf6c020bf69cf6692620ed4069c
Aditya Deshmukh <adityad1405@gmail.com>
2024-06-08 23:08:12 +0530

    vite config edit

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wordle/",
+  base: "/",
   plugins: [svelte({
     preprocess: vitePreprocess()
   })],
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: `[name]-v${version}.[ext]`,
-        entryFileNames: `[name]-v${version}.js`,
+        assetFileNames: `assets/[name]-v${version}.[ext]`,
+        entryFileNames: `assets/[name]-v${version}.js`,
         dir: "./dist",
       }
     }
